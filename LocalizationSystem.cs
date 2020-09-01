645fc4763306fb7d20389c7a616e4e07cde4ea06
Araujo, Jr <41656627+rmajunior@users.noreply.github.com>
2020-08-31 23:06:32 -0400

    Add files via upload

---
+++ LocalizationSystem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalizationSystem
+{
+  public enum Language
+  {
+        English,
+        Spanish
+  }
+
+    public static Language language = Language.Spanish;
+
+    private static Dictionary<string, string> localizedEN;
+    private static Dictionary<string, string> localizedSP;
+
+    public static bool isInit;
+
+    public static void Init()
+    {
+        CSVLoader csvLoader = new CSVLoader();
+        csvLoader.LoadCSV();
+
+        localizedEN = csvLoader.GetDictionaryValues("en");
+        localizedSP = csvLoader.GetDictionaryValues("sp");
+
+        isInit = true;               
+    }
+
+    public static string GetLocalizedValue(string key)
+    {
+        if(!isInit) { Init(); }
+
+        string value = key;
+
+        switch (language)
+        {
+            case Language.English:
+                localizedEN.TryGetValue(key, out value);
+                break;
+            case Language.Spanish:
+                localizedSP.TryGetValue(key, out value);
+                break;
+        }
+
+        return value;
+
+    }
+}
